b9607f7d1bb9051a58d49be772505aac5aeeb7ca
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:22:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user
+//after they log in to a website. Loop through the array, and print a greeting to each user:
+//* If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+//* Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+Object.defineProperty(exports, "__esModule", { value: true });
+//let userNames = ["dua","sakina","riffat","sumbul","saba"];
+//userNames.forEach(oneUser => {
+// if(oneUser === "Admin"){
+//  console.log(`Hello admin ${oneUser}, would you like to see a status report?`);
+// }else{
+//   console.log(`Hello ${oneUser}, thank you for logging in again.`);
+//};
+//});
+let usernames = ["dua", "sakina", "riffat", "sumbul", "saba"];
+for (let username of usernames) {
+    if (username === 'admin') {
+        console.log(`Hello admin, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    }
+}
